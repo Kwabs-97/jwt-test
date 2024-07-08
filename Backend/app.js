b65bcb18f5367104b779b40b916b9713a7b9d4a1
Kwabs-github <107767920+Kwabs-97@users.noreply.github.com>
2024-07-08 13:42:28 +0000

    route handlers, controllers, bodyparser for backend in progress

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,14 @@
 import express from "express";
 import { client } from "./config/postgresDB.js";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json());
+
 try {
   await client.connect();
   console.log("connected to db successfully");
