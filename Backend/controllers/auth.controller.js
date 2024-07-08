74011a7ee2fd63794e418bafba5280ba7b3759a8
Kwabs-github <107767920+Kwabs-97@users.noreply.github.com>
2024-07-08 23:17:58 +0000

    data successfully uploaded to database

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,5 +1,32 @@
+import bcrypt from "bcrypt";
+import {
+  _createNewUser,
+  _getUserByEmail,
+} from "../models/users/users.model.js";
 export async function registerController(req, res) {
   const { firstname, lastname, email, password } = req.body;
-  res.json({ firstname, lastname, email, password });
-  console.log(firstname, lastname, email, password);
+  try {
+    const existingUser = await _getUserByEmail(email);
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "User with this email already exist" });
+    }
+    const saltRounds = 10;
+    const hashed_password = bcrypt.hash(password, saltRounds);
+    const newUser = await _createNewUser(
+      firstname,
+      lastname,
+      email,
+      hashed_password,
+    );
+    res.status(200).json({
+      message: "user registration successful",
+      data: { firstname, lastname, email },
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: error.message });
+  }
 }
--- Backend/models/users/users.model.js
+++ Backend/models/users/users.model.js
@@ -1,17 +1,17 @@
-import { client } from "../../config/postgresDB/js";
+import { client } from "../../config/postgresDB.js";
 export async function _createNewUser(
-  first_name,
-  last_name,
+  firstname,
+  lastname,
   email,
   hashed_password,
 ) {
-  const createUserQuery = `INSERT INTO users (first_name, last_name, email, hashed_password)
+  const createUserQuery = `INSERT INTO users (firstname, lastname, email, hashed_password)
 VALUES($1,$2,$3,$4) RETURNING *
 `;
   try {
     const result = await client.query(createUserQuery, [
-      first_name,
-      last_name,
+      firstname,
+      lastname,
       email,
       hashed_password,
     ]);
@@ -22,10 +22,9 @@ VALUES($1,$2,$3,$4) RETURNING *
 }
 
 export async function _getUserByEmail(email) {
-  const getUserQuery = `SELECT * FROM users WHERE email = $1 RETURNING *`;
+  const getUserQuery = `SELECT * FROM users WHERE email = $1 `;
   try {
-    const result = client.query(getUserQuery, [email]);
-    return result.rows[0];
+    const result = await client.query(getUserQuery, [email]);
   } catch (error) {
     console.log("error finding user with this email", error);
   }
